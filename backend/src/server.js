3e2fb3270abd858ac6b8ae4fe58fd3dc400315b0
Rijen <rijenbhauju11@gmail.com>
2026-05-09 15:25:49 +0545

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv, { config } from "dotenv";
 dotenv.config();
+import path from "path";
 
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
@@ -13,18 +14,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname =path.resolve()
 
 app.use(express.json());
 
 
 
 //middleware
-app.use(express.json()); // This middleware will parse JSON boides : req.body
-app.use(
+if(process.env.NODE_ENV !== "prodcution") {
+    app.use(
     cors({
     origin: "http://localhost:5173",
     })
 );
+
+}
+app.use(express.json()); // This middleware will parse JSON boides : req.body
+
 app.use(rateLimiter);
 
 //our simple custom middleware
@@ -34,6 +40,15 @@ app.use(rateLimiter);
 //});
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.htm;"));
+})
+
+}
 
 connectDB().then(()=> app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
